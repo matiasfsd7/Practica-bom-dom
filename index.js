f5259c0c4ea42aae63a8912bd1f39ce7730dc3ce
matiasfsd7 <matias.n.ortega@gmail.com>
2023-06-19 20:15:58 -0300

    comence el maquetado y lofica

---
+++ index.js
@@ -0,0 +1,23 @@
+let min = 1;
+let max = 100;
+
+const buttonId1 = document.getElementById("buttonId1");
+buttonId1.addEventListener("click", () => {
+  aleatoreoNum();
+});
+function aleatoreoNum() {
+  let num = Math.floor(Math.random() * 101);
+  console.log(num);
+}
+aleatoreoNum();
+
+const buttonId2 = document.getElementById("button2");
+const number = document.getElementById("number");
+
+// function aleatoreoNum(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// console.log(aleatoreoNum(1, 100));
+// aleatoreoNum();
