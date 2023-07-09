9b59d5adace37b0c2dc89917b2e6607666b6839c
matiasfsd7 <matias.n.ortega@gmail.com>
2023-07-08 22:55:10 -0300

    Juego Terminado

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const buttoncomenzarJuego = document.getElementById("buttoncomenzarJuego");
+
 buttoncomenzarJuego.addEventListener("click", () => {
+  num = aleatoreoNum(1, 101);
+  console.log(num);
   buttoncomenzarJuego.innerHTML = "El juego comenzo 👀🤔 ";
   buttoncomenzarJuego.disabled = true;
-
-  num = aleatoreoNum(1, 100);
-  console.log(num);
+  const formJuego = document.getElementById("formuJuego");
+  formJuego.reset();
 });
+let num;
+
 function aleatoreoNum(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
-// let num = Math.floor(Math.random() * 101);
 
-let buttonEnviar = document.getElementById("buttonEnviar");
-
-let input = document.querySelector("input");
-
-// function adivinaNumero() {
-//   if (input == num) {
-//     alert("ADIVINASTE!");
-//   }
-//   if (input > num) {
-//     alert(" El numero es menor");
-//   } else input < num;
-//   alert("El numero es mayor");
-// }
+function consultarNumero(e) {
+  e.preventDefault();
+  let input = document.querySelector("input").value;
+  if (input == num) {
+    alert("ADIVINASTE");
+    buttoncomenzarJuego.innerHTML = "Vuelve a jugar";
+    buttoncomenzarJuego.disabled = false;
+  } else if (input > num) {
+    alert("Vuelve a intentarlo, el numero magico es menor al ingresado");
+  } else {
+    alert("Vuelve a intentarlo, el numero magico es mayor al ingresado");
+  }
+}
